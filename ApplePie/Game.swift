692536b21552356496ec2404dad9998044c7511e
Anish Gurusankar <anish.agclg@gmail.com>
2025-09-02 00:27:44 +0530

    refactored a func from ViewController.swift

---
+++ ApplePie/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Anish  on 02/09/25.
+//
+struct Game {
+    var word: String
+    var incorrectMovesRemaing: Int
+    var guessedLetters: [Character]
+}
+
