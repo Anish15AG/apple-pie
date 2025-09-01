9feaafd592fdedc4ac322a15aea14eb76556b44f
Anish Gurusankar <anish.agclg@gmail.com>
2025-09-02 00:15:14 +0530

    created outlets and actionFunction

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+var listOfWords = ["buccaneer", "swift", "glorius", "incandescent", "bug", "program"]
+
+let incorrectMovesAllowed: Int = 6
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func letterButtonAction(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
