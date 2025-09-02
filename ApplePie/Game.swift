37fed8cba7e571a88a950e1fec47dda6ca3d54d0
Anish Gurusankar <anish.agclg@gmail.com>
2025-09-02 08:41:14 +0530

    feat: playerGuessing function and formattedWord

--- ApplePie/Game.swift
+++ ApplePie/Game.swift
@@ -8,5 +8,25 @@ struct Game {
     var word: String
     var incorrectMovesRemaing: Int
     var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaing -= 1
+        }
+        
+    }
+    
+    var formattedWord: String {
+        var guessedWord: String = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "-"
+            }
+        }
+        return guessedWord
+    }
 }
 
